f602b9a651d204a20b79aca6983310d497ce9fe2
Philip Lysenko <philip.lysenko@conceptpeople.de>
2019-03-30 22:15:42 +0100

    changed main.ts for hmr support

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -1,13 +1,21 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { BootstrapModuleFn, hmr, WebpackModule } from '@ngxs/hmr-plugin';
+
+/* tslint:disable */
+declare const module: WebpackModule;
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+const bootstrap: BootstrapModuleFn = () => platformBrowserDynamic().bootstrapModule(AppModule);
+
+// see https://ngxs.gitbook.io/ngxs/plugins/hmr
+if (environment.hmr) {
+  hmr(module, bootstrap).catch(err => console.error(err));
+} else {
+  bootstrap().catch(err => console.log(err));
+}
