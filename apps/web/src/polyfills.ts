ced18929d84e804de817192b713ef2d460ce4ab9
Philip Lysenko <philip.lysenko@conceptpeople.de>
2019-03-31 08:25:59 +0200

    enabled hmr

--- apps/web/src/polyfills.ts
+++ apps/web/src/polyfills.ts
@@ -18,6 +18,10 @@
  * BROWSER POLYFILLS
  */
 
+/** Evergreen browsers require these. */
+// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
+// import 'core-js/es7/reflect';
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
