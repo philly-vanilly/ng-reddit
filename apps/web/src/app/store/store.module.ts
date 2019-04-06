3a11d776a2825bc31c0f6c663959d79479ef3a20
Philip Lysenko <philip.lysenko@conceptpeople.de>
2019-04-06 13:28:01 +0200

    removed development mode because of immutability bug in ngxs entity and removed karma tests (external lib)

--- apps/web/src/app/store/store.module.ts
+++ apps/web/src/app/store/store.module.ts
@@ -13,11 +13,13 @@ const states = [PostState, SubState];
 @NgModule({
   imports: [
     NgxsStoragePluginModule.forRoot({ // needs to be imported before NgxsModule to be available on initialization
-      key: ['auth'], // string or array of strings that can be deeply nested via dot notation
+      key: ['auth'], // string or array of strings that can be deeply nested via dot notation, eg 'auth.user'
       storage: StorageOption.SessionStorage
     }),
     NgxsModule.forRoot(states, {
-      developmentMode: !environment.production
+      // developmentMode freezes the store and makes JS objects immutable which doesn't work with Entity ATM
+      // https://github.com/ngxs-labs/entity-state/issues/52
+      developmentMode: false // !environment.production
     }),
     NgxsRouterPluginModule.forRoot(),
     NgxsReduxDevtoolsPluginModule.forRoot({
